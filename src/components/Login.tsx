8b6c841f3b98683c6f0b1020c1857c222ab23fa4
Rory How <rhow93@gmail.com>
2017-12-05 17:21:13 +0200

    add basic login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,51 @@
 import * as React from 'react';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
 
-export default class Login extends React.Component {
+interface State {
+  username: string;
+  password: string;
+}
+
+export default class Login extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleChange = (name: string) => (value: string) => {
+    if (name === 'username') {
+      return this.setState({
+        username: value
+      });
+    }
+    return this.setState({
+      password: value
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h1>Please log in</h1>
-      </div>
+      <form noValidate={true} autoComplete="off">
+        <TextField
+          label="Username"
+          value={this.state.username}
+          onChange={e => this.handleChange('username')(e.target.value)}
+          margin="normal"
+        />
+
+        <TextField
+          label="Password"
+          value={this.state.password}
+          type="password"
+          onChange={e => this.handleChange('password')(e.target.value)}
+          margin="normal"
+        />
+        <Button onClick={() => console.log('u wot m8')}>Submit</Button>
+      </form>
     );
   }
 }
