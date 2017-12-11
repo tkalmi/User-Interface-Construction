91bc757a68cb7978e069e76ee324aab13039ff43
Tomi Kalmi <tomi.kalmi@futurice.com>
2017-12-11 15:28:25 +0200

    Use material-ui Cards to give `AppointmentComponent` structure

--- src/components/AppointmentComponent.tsx
+++ src/components/AppointmentComponent.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import Appointment from '../types/Appointment';
+import Button from 'material-ui/Button';
+import Card, { CardContent } from 'material-ui/Card';
+import '../AppointmentComponent.css';
 
 interface Props {
   appointment: Appointment;
@@ -8,21 +11,27 @@ interface Props {
 const AppointmentComponent = (props: Props): JSX.Element => {
   const { title, date, time, location } = props.appointment;
   return (
-    <div>
-      <p>{date.format('dddd DD.MM.YYYY')}</p>
-      <p>{time}</p>
-      <p>{title}</p>
-      <p>{location}</p>
-      <a
-        target="_blank"
-        href={
-          'https://www.google.com/maps/search/?api=1&query=' +
-          encodeURIComponent(location)
-        }
-      >
-        Map
-      </a>
-    </div>
+    <Card className="appointment-card">
+      <CardContent>
+        <p>{date.format('dddd DD.MM.YYYY')}</p>
+        <p>{time}</p>
+        <p>{title}</p>
+        <p>{location}</p>
+        <div className="appointment-card-button">
+          <Button dense={true} color="primary">
+            <a
+              target="_blank"
+              href={
+                'https://www.google.com/maps/search/?api=1&query=' +
+                encodeURIComponent(location)
+              }
+            >
+              Map
+            </a>
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
