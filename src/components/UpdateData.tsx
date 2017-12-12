2eab4a3f3527869decb7470ba4feda1238dc1fdf
Olli Lehikoinen <olli.lehikoinen@aalto.fi>
2017-12-13 01:33:49 +0200

    Finished the data edit tab

--- src/components/UpdateData.tsx
+++ src/components/UpdateData.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react';
 import Paper from 'material-ui/Paper';
 import Tabs, { Tab } from 'material-ui/Tabs';
-import PhoneIcon from 'material-ui-icons/Phone';
-import FavoriteIcon from 'material-ui-icons/Favorite';
-import PersonPinIcon from 'material-ui-icons/PersonPin';
-//import Typography from 'material-ui/Typography';
+import CreateIcon from 'material-ui-icons/Create';
+import FitnessIcon from 'material-ui-icons/FitnessCenter';
+import StepsIcon from 'material-ui-icons/DirectionsRun';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import MoodIcon from 'material-ui-icons/Mood';
+import MoodBadIcon from 'material-ui-icons/MoodBad';
+import SatisfiedIcon from 'material-ui-icons/SentimentSatisfied';
+import DissatisfiedIcon from 'material-ui-icons/SentimentDissatisfied';
+import NeutralIcon from 'material-ui-icons/SentimentNeutral';
+import IconButton from 'material-ui/IconButton';
+import '../UpdateDataComponent.css';
 
-/**function TabContainer(props: any) {
-    return (
-        <Typography component="div" style={{ padding: 8 * 3 }}>
-            {props.children}
-        </Typography>
-    );
-}**/
+function TabContainer(props: any) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
 
 export default class IconLabelTabs extends React.Component {
   state = {
@@ -24,6 +32,8 @@ export default class IconLabelTabs extends React.Component {
   };
 
   render() {
+    const { value } = this.state;
+
     return (
       <Paper style={{ width: 500 }}>
         <Tabs
@@ -33,11 +43,104 @@ export default class IconLabelTabs extends React.Component {
           indicatorColor="accent"
           textColor="accent"
         >
-          <Tab icon={<PhoneIcon />} label="RECENTS" />
-          <Tab icon={<FavoriteIcon />} label="FAVORITES" />
-          <Tab icon={<PersonPinIcon />} label="NEARBY" />
+          <Tab icon={<CreateIcon />} label="EDIT" />
+          <Tab icon={<FitnessIcon />} label="WEIGHT" />
+          <Tab icon={<StepsIcon />} label="STEPS" />
         </Tabs>
+        {value === 0 && (
+          <TabContainer>
+            <DataTab />
+          </TabContainer>
+        )}
+        {value === 1 && <TabContainer>Item Two</TabContainer>}
+        {value === 2 && <TabContainer>Item Three</TabContainer>}
       </Paper>
+    );
+  }
+}
+
+class DataTab extends React.Component<{}, { weight: number; height: number }> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      weight: 60,
+      height: 160
+    };
+  }
+  weightAdjust(i: number) {
+    const weight = this.state.weight;
+    this.setState({ weight: Math.round((weight + i) * 10) / 10 });
+  }
+  heightAdjust(i: number) {
+    const weight = this.state.height;
+    this.setState({ height: Math.round((weight + i) * 10) / 10 });
+  }
+
+  render() {
+    return (
+      <div>
+        <h4 className="tab-center">Weight</h4>
+        <div className="tab-center">
+          <Button color="primary" onClick={() => this.weightAdjust(1)}>
+            + 1
+          </Button>
+          <Button color="primary" onClick={() => this.weightAdjust(0.1)}>
+            + 0.1
+          </Button>
+          <b className="row-margin">{this.state.weight}</b>
+          <Button color="accent" onClick={() => this.weightAdjust(-0.1)}>
+            - 0.1
+          </Button>
+          <Button color="accent" onClick={() => this.weightAdjust(-1)}>
+            - 1
+          </Button>
+        </div>
+        <h4 className="tab-center">Height</h4>
+        <div className="tab-center">
+          <Button color="primary" onClick={() => this.heightAdjust(1)}>
+            + 1
+          </Button>
+          <Button color="primary" onClick={() => this.heightAdjust(0.1)}>
+            + 0.1
+          </Button>
+          <b className="row-margin">{this.state.height}</b>
+          <Button color="accent" onClick={() => this.heightAdjust(-0.1)}>
+            - 0.1
+          </Button>
+          <Button color="accent" onClick={() => this.heightAdjust(-1)}>
+            - 1
+          </Button>
+        </div>
+        <h4 className="tab-center">How are you feeling?</h4>
+        <div className="tab-center">
+          <IconButton>
+            <MoodBadIcon />
+          </IconButton>
+          <IconButton>
+            <DissatisfiedIcon />
+          </IconButton>
+          <IconButton>
+            <NeutralIcon />
+          </IconButton>
+          <IconButton>
+            <SatisfiedIcon />
+          </IconButton>
+          <IconButton>
+            <MoodIcon />
+          </IconButton>
+        </div>
+        <h4 className="tab-center">Did you eat healthy?</h4>
+        <div className="tab-center">
+          <Button color="primary">Yes</Button>
+          <Button color="accent">No</Button>
+        </div>
+        <br />
+        <div className="tab-center">
+          <Button raised color="primary">
+            Done
+          </Button>
+        </div>
+      </div>
     );
   }
 }
