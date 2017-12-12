d9fa87b5274002ddcd81d255b80fc9eb0c9ab3af
Tomi Kalmi <tomi.kalmi@futurice.com>
2017-12-12 19:10:52 +0200

    Implement basic `AppBar` component and add it to all views

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,28 @@ import Login from './components/Login';
 import AppointmentList from './components/AppointmentList';
 import TimeReservation from './components/TimeReservation';
 import Home from './components/Home';
+import AppBar from './components/AppBar';
 
 const App = (): JSX.Element => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact={true} path="/login" component={Login} />
-        <Route exact={true} path="/appointments" component={AppointmentList} />
-        <Route
-          exact={true}
-          path="/timeReservation"
-          component={TimeReservation}
-        />
-        <Route path="/*" component={Home} />
-      </Switch>
-    </BrowserRouter>
+    <AppBar>
+      <BrowserRouter>
+        <Switch>
+          <Route exact={true} path="/login" component={Login} />
+          <Route
+            exact={true}
+            path="/appointments"
+            component={AppointmentList}
+          />
+          <Route
+            exact={true}
+            path="/timeReservation"
+            component={TimeReservation}
+          />
+          <Route path="/*" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </AppBar>
   );
 };
 
---
+++ src/components/AppBar.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+
+const styles = {
+  root: {
+    width: '100%'
+  },
+  flex: {
+    flex: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+};
+
+function ButtonAppBar(props: any) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography type="title" color="inherit" className={classes.flex}>
+            Healthbot 3000
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {props.children}
+    </div>
+  );
+}
+
+export default withStyles(styles)(ButtonAppBar);
